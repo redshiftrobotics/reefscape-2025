43699024a8ddd394fe2f41cc5e9b68d7e4ffe8f6
WookieRUs <nathanielrock@seattleacademy.org>
2025-01-15 16:51:19 -0800

    Autos
    25+ paths and auto paths from Path Planner, plus other files. All finished Auto paths are for dropping a coral in L1, going to pickup another, and putting another coral in L1. Paths for the Auto paths as well

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,12 +6,12 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "reefscape-2025";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "84e4c133d75128cc9f414e835ad964327d355515";
-  public static final String GIT_DATE = "2025-01-06 23:26:32 PST";
+  public static final String GIT_SHA = "0cdd1519a59bdd115027e2d58c0381b6bca1f177";
+  public static final String GIT_DATE = "2025-01-06 23:43:43 PST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-06 23:42:19 PST";
-  public static final long BUILD_UNIX_TIME = 1736235739361L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-08 18:00:30 PST";
+  public static final long BUILD_UNIX_TIME = 1736388030353L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
