785adeaa18620ac7c1909de049af3bc0c1e25126
Aceius E. <aceiuselms@seattleacademy.org>
2025-01-25 14:08:39 -0800

    Add new methods to Hang interface

--- src/main/java/frc/robot/subsystems/hang/HangIO.java
+++ src/main/java/frc/robot/subsystems/hang/HangIO.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.hang;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface HangIO {
@@ -9,4 +10,25 @@ public interface HangIO {
   }
 
   public void updateInputs(HangIOInputs inputs);
+
+  /**
+   * Set the desired angle for the arm to travel to.
+   *
+   * @param setpoint The desired angle for the arm to travel to.
+   */
+  public void setSetpoint(Rotation2d setpoint);
+
+  /**
+   * Get the desired angle for the arm to travel to.
+   *
+   * @return The angle the arm is currently traveling to.
+   */
+  public double getSetpoint();
+
+  /**
+   * Get the current position of the arm.
+   *
+   * @return The current position of the arm.
+   */
+  public double getPosition();
 }
