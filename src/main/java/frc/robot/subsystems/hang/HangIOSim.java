52a657962926a0135a4093bb3d58277786337f14
Aceius E. <aceiuselms@seattleacademy.org>
2025-01-25 13:41:34 -0800

    Fill in length value

--- src/main/java/frc/robot/subsystems/hang/HangIOSim.java
+++ src/main/java/frc/robot/subsystems/hang/HangIOSim.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.hang;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
 public class HangIOSim implements HangIO {
   // TODO
-  private SingleJointedArmSim sim = new SingleJointedArmSim(null, 0, 0, 0, 0, 0, false, 0, null);
+  private SingleJointedArmSim sim =
+      new SingleJointedArmSim(null, 0, 0, Units.inchesToMeters(18), 0, 0, false, 0, null);
 
   @Override
   public void updateInputs(HangIOInputs inputs) {
