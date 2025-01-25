8165be236bf465cba76e5755b386858e9c7fb989
Aceius E. <aceiuselms@seattleacademy.org>
2025-01-25 11:40:37 -0800

    Scaffhold sim implementation

---
+++ src/main/java/frc/robot/subsystems/hang/HangIOSim.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.hang;
+
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
+public class HangIOSim implements HangIO {
+  // TODO
+  private SingleJointedArmSim sim = new SingleJointedArmSim(null, 0, 0, 0, 0, 0, false, 0, null);
+
+  @Override
+  public void updateInputs(HangIOInputs inputs) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
+  }
+}
