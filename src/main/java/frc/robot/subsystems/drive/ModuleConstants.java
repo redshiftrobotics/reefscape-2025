642e33caf2f2badb5512c9e0f588dd7ec58912b4
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2025-01-27 11:00:14 -0800

    remove old line

--- src/main/java/frc/robot/subsystems/drive/ModuleConstants.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleConstants.java
@@ -123,7 +123,6 @@ public class ModuleConstants {
         DRIVE_MOTOR = DCMotor.getNEO(1);
         DRIVE_FEEDBACK = new PIDConstants(0.0001, 0.0, 0.0);
         DRIVE_FEED_FORWARD = new FeedForwardConstants(0.1, 2.35, 0.53);
-        // DRIVE_FEED_FORWARD = new FeedForwardConstants(0.0, 0.0, 0.0);
         DRIVE_MOTOR_CURRENT_LIMIT = 50;
         DRIVE_REDUCTION = Mk4iReductions.L3.reduction;
 
