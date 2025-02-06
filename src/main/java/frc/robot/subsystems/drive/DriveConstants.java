a480e467ce82c82a2e727fef18db5176378c11ee
leifborwick <163786886+leifborwick@users.noreply.github.com>
2025-02-06 15:42:37 -0800

    Merge branch 'main' into elevator
    Ok why is elevator messing with vision stuff so much? Mostly accepted main.

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.drive;
 
 import com.pathplanner.lib.path.PathConstraints;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
@@ -55,9 +57,14 @@ public class DriveConstants {
 
   public static final DriveConfig DRIVE_CONFIG =
       switch (Constants.getRobot()) {
-        case WOOD_BOT_TWO_2025, T_SHIRT_CANNON_CHASSIS -> new DriveConfig(
+        case T_SHIRT_CANNON_CHASSIS -> new DriveConfig(
             new Translation2d(Units.inchesToMeters(22.729226), Units.inchesToMeters(22.729226)),
             new Translation2d(Units.inchesToMeters(25.729226), Units.inchesToMeters(25.729226)),
+            5.05968,
+            14.5);
+        case WOOD_BOT_TWO_2025 -> new DriveConfig(
+            new Translation2d(Units.inchesToMeters(22.729226), Units.inchesToMeters(22.729226)),
+            new Translation2d(Units.inchesToMeters(27.5), Units.inchesToMeters(27.5)),
             5.05968,
             14.5);
         case CRESCENDO_CHASSIS_2024 -> new DriveConfig(
@@ -118,6 +125,8 @@ public class DriveConstants {
 
   public static final PIDConstants TRANSLATION_CONTROLLER_CONSTANTS = new PIDConstants(5.0, 0.0, 1);
   public static final PIDConstants ROTATION_CONTROLLER_CONSTANTS = new PIDConstants(5.0, 0, 1);
+  public static final Pose2d POSE_POSITION_TOLERANCE =
+      new Pose2d(Units.inchesToMeters(0.5), Units.inchesToMeters(0.5), Rotation2d.fromDegrees(1));
 
   // --- Heading Controller Config ---
 
