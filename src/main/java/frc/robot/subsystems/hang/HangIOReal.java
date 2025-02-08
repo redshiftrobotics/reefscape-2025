c2fac7aa9338fad6676db074b7ff805d46830719
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-08 13:43:11 -0800

    Get started on real hangIO implementation

---
+++ src/main/java/frc/robot/subsystems/hang/HangIOReal.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems.hang;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.controller.PIDController;
+
+public class HangIOReal implements HangIO {
+  SparkMax wenchMotor = new SparkMax(0, MotorType.kBrushless); // TODO CAN ID
+
+  PIDController controller = new PIDController(0, 0, 0); // TODO Tune
+
+  double setpoint = 0.0;
+
+  @Override
+  public void updateInputs(HangIOInputs inputs) {
+    wenchMotor.set(controller.calculate(getPosition(), setpoint));
+
+    inputs.armSetpoint = setpoint;
+    inputs.armPosition = getPosition();
+  }
+
+  @Override
+  public void setSetpoint(double setpoint) {
+    this.setpoint = setpoint;
+  }
+
+  @Override
+  public double getSetpoint() {
+    return setpoint;
+  }
+
+  @Override
+  public double getPosition() {
+    return wenchMotor.getAbsoluteEncoder().getPosition();
+  }
+}
