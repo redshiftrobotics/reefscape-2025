17da2bd61ec183a348379b4b2fed255d4597ad8a
EthanElms <ethanelms@seattleacademy.org>
2025-02-08 13:54:56 -0800

    subsytem done exept for sensor + fixed names

--- src/main/java/frc/robot/subsystems/Intake/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/Intake/CoralIntake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.Intake;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralIntake extends SubsystemBase {
@@ -8,5 +9,13 @@ public class CoralIntake extends SubsystemBase {
 
   public CoralIntake(CoralIntakeIO io) {
     this.io = io;
+  }
+
+    public void MoveLeftMotor(double Speed) {
+    io.MoveLeftMotor(Speed);
+  }
+
+  public void MoveRightMotor(double Speed) {
+    io.MoveRightMotor(Speed);
   }
 }
