12e0dacf1c3310eb9df77238112e96629ea58c05
EthanElms <ethanelms@seattleacademy.org>
2025-02-08 15:31:37 -0800

    Intake constants done

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.Intake;
+
+import com.revrobotics.spark.SparkMax;
+
+public class IntakeConstants {
+  public static final int LeftMotorID = 0;
+  public static final int RightMotorID = 0;
+}
