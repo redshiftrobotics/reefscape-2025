1dc19458cf94ed5de2cd8d01c9b5ea0666c75af7
EthanElms <ethanelms@seattleacademy.org>
2025-02-08 14:33:24 -0800

    ADD bindings

--- src/main/java/frc/robot/subsystems/Intake/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/Intake/CoralIntake.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class CoralIntake extends SubsystemBase {
 
   // !! SENSORS NOT IMPLMENTED !! \\
-
+//no bindings
   private CoralIntakeIO io;
 
   public CoralIntake(CoralIntakeIO io) {
