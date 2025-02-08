0536c7bd0e9098fb8bbfd4a0c9ce7df6a8b69937
EthanElms <ethanelms@seattleacademy.org>
2025-02-08 14:12:00 -0800

    dependencys injected to command+random stuff

--- src/main/java/frc/robot/commands/Intake/ScoreCoral.java
+++ src/main/java/frc/robot/commands/Intake/ScoreCoral.java
@@ -1,5 +1,22 @@
 package frc.robot.commands.Intake;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake.CoralIntake;
 
-public class ScoreCoral extends Command {}
+public class ScoreCoral extends Command {
+
+  private Joystick joystick;
+  private CoralIntake coralIntake;
+
+  public ScoreCoral(Joystick joystick,CoralIntake coralIntake) {
+    this.joystick = joystick;
+    this.coralIntake = coralIntake;
+  }
+
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
--- src/main/java/frc/robot/subsystems/Intake/CoralIntakeIO.java
+++ src/main/java/frc/robot/subsystems/Intake/CoralIntakeIO.java
@@ -6,20 +6,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralIntakeIO extends SubsystemBase {
 
-  private SparkMax LeftMotor;
-  private SparkMax RightMotor;
+  private SparkMax leftMotor;
+  private SparkMax rightMotor;
 
   public CoralIntakeIO(SparkMax LeftMotor, SparkMax rightMax) {
 
-    this.LeftMotor = LeftMotor;
-    this.RightMotor = RightMotor;
+    this.leftMotor = LeftMotor;
+    this.rightMotor = rightMotor;
   }
 
+  //motor stuff
+
   public void MoveLeftMotor(double Speed) {
-    LeftMotor.set(MathUtil.clamp(Speed, -0.5, 0.5));
+    leftMotor.set(MathUtil.clamp(Speed, -0.5, 0.5));
   }
 
   public void MoveRightMotor(double Speed) {
-    RightMotor.set(MathUtil.clamp(Speed, -0.5, 0.5));
+    rightMotor.set(MathUtil.clamp(Speed, -0.5, 0.5));
   }
+
+  //TODO add the sensor stuff
 }
