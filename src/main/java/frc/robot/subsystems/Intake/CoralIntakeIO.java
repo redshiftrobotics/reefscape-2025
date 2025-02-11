81589c063b135563bb311958f924170c461eb1ba
EthanElms <ethanelms@seattleacademy.org>
2025-02-10 16:58:05 -0800

    changed way inputs and motors made +working on sensor check

--- src/main/java/frc/robot/subsystems/Intake/CoralIntakeIO.java
+++ src/main/java/frc/robot/subsystems/Intake/CoralIntakeIO.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.Intake;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralIntakeIO extends SubsystemBase {
@@ -9,10 +12,14 @@ public class CoralIntakeIO extends SubsystemBase {
   private SparkMax leftMotor;
   private SparkMax rightMotor;
 
-  public CoralIntakeIO(SparkMax LeftMotor, SparkMax rightMax) {
+  private AnalogInput analogInput;
 
-    this.leftMotor = LeftMotor;
-    this.rightMotor = rightMotor;
+  public CoralIntakeIO(int leftMotor, int rightMotor, int analogInput) {
+
+    this.leftMotor = new SparkMax(leftMotor, MotorType.kBrushless);
+    this.rightMotor = new SparkMax(rightMotor, MotorType.kBrushless);
+
+    this.;
   }
 
   // motor stuff
@@ -24,6 +31,14 @@ public class CoralIntakeIO extends SubsystemBase {
   public void moveRightMotor(double Speed) {
     rightMotor.set(MathUtil.clamp(Speed, -0.5, 0.5));
   }
+
+  public boolean checkSensor() {
+    
+    
+    
+    return true;
+  }
+
 
   public void stop() {
     leftMotor.stopMotor();
