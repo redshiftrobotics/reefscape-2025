55ca06865ac4c33f48b5133e6747e3cbc0ea80df
MichaelLesirge <michael.lesirge@gmail.com>
2025-02-13 22:09:37 -0800

    fixes after merge

--- src/main/java/frc/robot/commands/AdaptiveAutoAlignCommands.java
+++ src/main/java/frc/robot/commands/AdaptiveAutoAlignCommands.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import static frc.robot.subsystems.drive.DriveConstants.DRIVE_CONFIG;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
