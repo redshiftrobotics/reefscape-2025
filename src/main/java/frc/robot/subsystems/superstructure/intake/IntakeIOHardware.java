aed96438c6f7b235868b90ad6eb979fa682b2af9
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-15 13:55:56 -0800

    Mostly implement hardware

--- src/main/java/frc/robot/subsystems/superstructure/intake/IntakeIOHardware.java
+++ src/main/java/frc/robot/subsystems/superstructure/intake/IntakeIOHardware.java
@@ -1,16 +1,43 @@
 package frc.robot.subsystems.superstructure.intake;
 
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 
 /** Hardware implementation of the TemplateIO. */
 public class IntakeIOHardware implements IntakeIO {
-  private SparkMax motor1;
-  private SparkMax motor2;
+  private SparkMax leftMotor;
+  private SparkMax rightMotor;
 
-  public IntakeIOHardware(int motorId1, int motorId2)
-  {
-      motor1 = new SparkMax(motorId1, MotorType.kBrushless);
-      motor2 = new SparkMax(motorId2, MotorType.kBrushless);
+  public IntakeIOHardware(int motorIdLeft, int motorIdRight) {
+    leftMotor = new SparkMax(motorIdLeft, MotorType.kBrushless);
+    rightMotor = new SparkMax(motorIdRight, MotorType.kBrushless);
+  }
+
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {
+    inputs.speedLeft = leftMotor.get();
+    inputs.speedRight = rightMotor.get();
+  }
+
+  @Override
+  public void setLeftMotor(double speed) {
+    leftMotor.set(speed);
+  }
+
+  @Override
+  public void setRightMotor(double speed) {
+    rightMotor.set(speed);
+  }
+
+  @Override
+  public boolean isOccupied() {
+    // TODO Auto-generated method stub
+    return IntakeIO.super.isOccupied();
+  }
+
+  @Override
+  public void stopMotors() {
+    leftMotor.stopMotor();
+    rightMotor.stopMotor();
   }
 }
