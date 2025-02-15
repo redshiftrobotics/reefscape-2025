e0c28c4546abc32a44d1e09e104a60e8a7487576
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-15 14:02:30 -0800

    Delete inferior implementation
    Co-Authored-By: EthanElms <187707577+EthanElms@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.intake;
-
-public class IntakeConstants {
-  public static final int LEFT_MOTOR_ID = 0;
-  public static final int RIGHT_MOTOR_ID = 0;
-  public static final int Sensor_ID = 0;
-}
