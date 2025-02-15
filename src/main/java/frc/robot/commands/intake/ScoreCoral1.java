f45c179b4f585ae26ddffc750c0d8399e98c5837
EthanElms <ethanelms@seattleacademy.org>
2025-02-15 11:28:42 -0800

    scores work ready to test

---
+++ src/main/java/frc/robot/commands/intake/ScoreCoral1.java
@@ -0,0 +1,31 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.CoralIntake;
+
+public class ScoreCoral1 extends Command {
+
+  private CoralIntake coralIntake;
+
+  public ScoreCoral1(CoralIntake coralIntake) {
+    this.coralIntake = coralIntake;
+
+    addRequirements(coralIntake);
+  }
+
+  @Override
+  public void execute() {
+    //scores on l1
+    coralIntake.moveRightMotor(0.3);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    coralIntake.stop();
+  }
+}
---
+++ src/main/java/frc/robot/commands/intake/ScoreCoral2.java
@@ -0,0 +1,32 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.CoralIntake;
+
+public class ScoreCoral2 extends Command {
+
+  private CoralIntake coralIntake;
+
+  public ScoreCoral2(CoralIntake coralIntake) {
+    this.coralIntake = coralIntake;
+
+    addRequirements(coralIntake);
+  }
+
+  @Override
+  public void execute() {
+    //score on not l1
+    coralIntake.moveRightMotor(0.3);
+    coralIntake.moveLeftMotor(0.3);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    coralIntake.stop();
+  }
+}
