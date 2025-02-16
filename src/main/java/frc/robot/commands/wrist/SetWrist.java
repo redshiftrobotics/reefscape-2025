4e69d6c1872537b6df3f53bc66752007f0f7b9f6
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-15 17:22:23 -0800

    SetWrist Command
    Co-Authored-By: EthanElms <187707577+EthanElms@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/wrist/SetWrist.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.wrist;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.superstructure.wrist.Wrist;
+
+public class SetWrist extends Command {
+
+  private double setpoint;
+  private Wrist wrist;
+
+  public SetWrist(Wrist wrist, double setpoint) {
+    this.setpoint = setpoint;
+    this.wrist = wrist;
+  }
+
+  @Override
+  public void initialize() {
+    wrist.goTo(setpoint);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return wrist.atSetpoint();
+  }
+}
