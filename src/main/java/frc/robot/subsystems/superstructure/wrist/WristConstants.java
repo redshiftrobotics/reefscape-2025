b5f73bf689e42fe35a2e6dc81bd93471dc97c601
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-15 16:35:26 -0800

    Angle constants

--- src/main/java/frc/robot/subsystems/superstructure/wrist/WristConstants.java
+++ src/main/java/frc/robot/subsystems/superstructure/wrist/WristConstants.java
@@ -1,4 +1,10 @@
 package frc.robot.subsystems.superstructure.wrist;
 
+import edu.wpi.first.math.util.Units;
+
 /** Constants for the Template subsystem. */
-public class WristConstants {}
+public class WristConstants {
+  // TODO Numbers are from game manual but need verificiation
+  public static final double CORAL_SCORING_POSITION = Units.degreesToRotations(35);
+  public static final double CORAL_PICKUP_POSITION = Units.degreesToRotations(55);
+}
