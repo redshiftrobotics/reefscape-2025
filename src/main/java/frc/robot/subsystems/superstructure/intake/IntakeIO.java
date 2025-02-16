fba895b3a306ecc2abb7ecb8718bb9569440526f
Aceius E. <144858100+AceiusRedshift@users.noreply.github.com>
2025-02-16 00:08:22 -0800

    wowwe joava has ugly array initiazltion

--- src/main/java/frc/robot/subsystems/superstructure/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/superstructure/intake/IntakeIO.java
@@ -8,6 +8,9 @@ public interface IntakeIO {
   public static class IntakeIOInputs {
     public double speedLeft;
     public double speedRight;
+    
+    public double[] appliedVolts = new double[] {};
+    public double[] supplyCurrentAmps = new double[] {};
   }
 
   /** Updates the set of loggable inputs. */
