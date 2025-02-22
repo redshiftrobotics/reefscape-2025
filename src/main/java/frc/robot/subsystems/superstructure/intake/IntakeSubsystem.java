04f8333f4c22df11df8a53ba2b116d7604ece51d
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-22 14:19:08 -0800

    spotless

--- src/main/java/frc/robot/subsystems/superstructure/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/superstructure/intake/IntakeSubsystem.java
@@ -4,7 +4,10 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface IntakeSubsystem extends Subsystem {
   public void setLeftMotor(double speed);
+
   public void setRightMotor(double speed);
+
   public void setMotors(double speed);
+
   public void stopMotors();
 }
