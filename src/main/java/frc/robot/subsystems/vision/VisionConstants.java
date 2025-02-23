8641c8504c60b2f851c1c5eb7b57c730c0f88367
MichaelLesirge <michael.lesirge@gmail.com>
2025-02-23 08:01:46 -0800

    vision sim test

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -10,14 +10,13 @@ import edu.wpi.first.math.util.Units;
 public class VisionConstants {
   // --- Vision Config ---
 
-  
   public static final AprilTagFieldLayout FIELD =
       AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
 
   // Set cameraName on PhotonVision web interface. Edit camera name from camera type to camera
   // position. To find robotToCamera, measure the distance from the camera to the center of the
   // robot or use the robot's CAD model.
-  
+
   // Docs: https://docs.photonvision.org/en/latest/docs/apriltag-pipelines/coordinate-systems.html
 
   public record CameraConfig(String cameraName, Transform3d robotToCamera) {}
@@ -44,45 +43,42 @@ public class VisionConstants {
                   0, -Units.inchesToMeters(27.5 / 2.0 + 1.0), Units.inchesToMeters(3)),
               new Rotation3d(0, Units.degreesToRadians(0), Units.degreesToRadians(-90))));
 
-
   private static final double CAMERA_OFFSET_X = Units.inchesToMeters(9.922);
   private static final double CAMERA_OFFSET_Y = Units.inchesToMeters(9.906);
-  
-  private static final double FRONT_CAMERA_YAW = Units.degreesToRadians(0);
+
+  private static final double FRONT_CAMERA_YAW = Units.degreesToRadians(-20);
   private static final double FRONT_CAMERA_PITCH = Units.degreesToRadians(20);
   private static final double FRONT_CAMERA_OFFSET_Z = Units.inchesToMeters(7.888);
-  
-  private static final double BACK_CAMERA_YAW = Units.degreesToRadians(0);
+
+  private static final double BACK_CAMERA_YAW = Units.degreesToRadians(45);
   private static final double BACK_CAMERA_PITCH = Units.degreesToRadians(10);
   private static final double BACK_CAMERA_OFFSET_Z = Units.inchesToMeters(8.285);
 
-  public static final CameraConfig COMP_FRONT_LEFT_CAMERA = 
+  public static final CameraConfig COMP_FRONT_LEFT_CAMERA =
       new CameraConfig(
           "FrontLeftCamera8032",
           new Transform3d(
               new Translation3d(+CAMERA_OFFSET_X, +CAMERA_OFFSET_Y, FRONT_CAMERA_OFFSET_Z),
-              new Rotation3d(0, FRONT_CAMERA_PITCH, FRONT_CAMERA_YAW)));
+              new Rotation3d(0, FRONT_CAMERA_PITCH, +FRONT_CAMERA_YAW)));
 
-  public static final CameraConfig COMP_FRONT_RIGHT_CAMERA = 
+  public static final CameraConfig COMP_FRONT_RIGHT_CAMERA =
       new CameraConfig(
           "FrontRightCamera8032",
           new Transform3d(
               new Translation3d(+CAMERA_OFFSET_X, -CAMERA_OFFSET_Y, FRONT_CAMERA_OFFSET_Z),
-              new Rotation3d(0, FRONT_CAMERA_PITCH, FRONT_CAMERA_YAW)));
+              new Rotation3d(0, FRONT_CAMERA_PITCH, -FRONT_CAMERA_YAW)));
 
-  public static final CameraConfig COMP_BACK_LEFT_CAMERA = 
+  public static final CameraConfig COMP_BACK_LEFT_CAMERA =
       new CameraConfig(
           "BackLeftCamera8032",
           new Transform3d(
               new Translation3d(-CAMERA_OFFSET_X, +CAMERA_OFFSET_Y, BACK_CAMERA_OFFSET_Z),
-              new Rotation3d(0, BACK_CAMERA_PITCH, BACK_CAMERA_YAW)));
+              new Rotation3d(0, BACK_CAMERA_PITCH, Units.degreesToRadians(180) - BACK_CAMERA_YAW)));
 
-  public static final CameraConfig COMP_BACK_RIGHT_CAMERA = 
+  public static final CameraConfig COMP_BACK_RIGHT_CAMERA =
       new CameraConfig(
           "BackRightCamera8032",
           new Transform3d(
               new Translation3d(-CAMERA_OFFSET_X, -CAMERA_OFFSET_Y, BACK_CAMERA_OFFSET_Z),
-              new Rotation3d(0, BACK_CAMERA_PITCH, BACK_CAMERA_YAW)));
-
-
+              new Rotation3d(0, BACK_CAMERA_PITCH, Units.degreesToRadians(180) + BACK_CAMERA_YAW)));
 }
