809bd3a2d5e5ded16664efd046c6d3a2c38df800
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-26 15:51:45 -0800

    Use factories

--- src/main/java/frc/robot/subsystems/superstructure/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/superstructure/wrist/Wrist.java
@@ -14,7 +14,6 @@ public class Wrist extends SubsystemBase {
   private static final LoggedTunableNumberFactory factory =
       new LoggedTunableNumberFactory("Elevator");
 
-  // TODO: These tuners are never used. You need to check if there are changed then update the PID controller.
   private static final LoggedTunableNumber kP = factory.getNumber("kP", REAL_P);
   private static final LoggedTunableNumber kI = factory.getNumber("kI", REAL_I);
   private static final LoggedTunableNumber kD = factory.getNumber("kD", REAL_D);
@@ -38,14 +37,22 @@ public class Wrist extends SubsystemBase {
   @Override
   public void periodic() {
     io.updateInputs(inputs);
+
     Logger.processInputs("Wrist", inputs);
 
-    currentWristVisualizer.setRotations(inputs.position);
-    targetWristVisualizer.setRotations(inputs.setpoint);
+    LoggedTunableNumber.ifChanged(
+        hashCode(),
+        (values) -> {
+          io.setPid(values[0], values[1], values[2]);
+        },
+        kP,
+        kI,
+        kD);
   }
 
   public void goTo(double setpoint) {
-    // TODO: Nitpick: I prefer to call this something like setGoalRotations(rotations) or setGoalPosition(degrees)
+    // TODO: Nitpick: I prefer to call this something like setGoalRotations(rotations) or
+    // setGoalPosition(degrees)
     // helpful to distinguish between the setpoint and the actual position
     io.goTo(setpoint);
   }
@@ -56,7 +63,8 @@ public class Wrist extends SubsystemBase {
 
   /** Get position in rotations */
   public double getPosition() {
-    // Nitpick again: I would call this something like get measured position or something like that, not important though just me
+    // Nitpick again: I would call this something like get measured position or something like that,
+    // not important though just me
     return inputs.position;
   }
 
