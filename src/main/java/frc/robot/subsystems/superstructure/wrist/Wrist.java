cc490d9bb1a8841d8892c408b3266d65d758bc62
Aceius E. <aceiuselms@seattleacademy.org>
2025-02-26 16:04:36 -0800

    Address wrist to-dos

--- src/main/java/frc/robot/subsystems/superstructure/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/superstructure/wrist/Wrist.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.superstructure.wrist;
 
 import static frc.robot.subsystems.superstructure.wrist.WristConstants.*;
 
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utility.tunable.LoggedTunableNumber;
 import frc.robot.utility.tunable.LoggedTunableNumberFactory;
@@ -10,7 +9,6 @@ import org.littletonrobotics.junction.Logger;
 
 /** Mechanism at end of elevator to move intake/ */
 public class Wrist extends SubsystemBase {
-
   private static final LoggedTunableNumberFactory factory =
       new LoggedTunableNumberFactory("Elevator");
 
@@ -20,8 +18,6 @@ public class Wrist extends SubsystemBase {
 
   private final WristIO io;
   private final WristIOInputsAutoLogged inputs = new WristIOInputsAutoLogged();
-
-  // TODO: This is more competant than the way its done in the hang arm
 
   /** Creates a new Wrist. */
   public Wrist(WristIO io) {
@@ -44,10 +40,8 @@ public class Wrist extends SubsystemBase {
         kD);
   }
 
-  public void goTo(double setpoint) {
-    // TODO: Nitpick: I prefer to call this something like setGoalRotations(rotations) or
-    // setGoalPosition(degrees)
-    // helpful to distinguish between the setpoint and the actual position
+  /** In rotations. */
+  public void setGoal(double setpoint) {
     io.goTo(setpoint);
   }
 
@@ -56,9 +50,7 @@ public class Wrist extends SubsystemBase {
   }
 
   /** Get position in rotations */
-  public double getPosition() {
-    // Nitpick again: I would call this something like get measured position or something like that,
-    // not important though just me
+  public double getMeasuredPosition() {
     return inputs.position;
   }
 
