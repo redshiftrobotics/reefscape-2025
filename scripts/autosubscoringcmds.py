85ecb5cb60d2060996cae1b7982579177d9a4241
RobotLeopard86 <63123751+RobotLeopard86@users.noreply.github.com>
2025-03-03 16:09:32 -0800

    Add PathPlanner command substitution and rotation adjustment scripts

---
+++ scripts/autosubscoringcmds.py
@@ -0,0 +1,28 @@
+import json
+import sys
+import pathlib
+
+if len(sys.argv) < 2:
+	raise SystemExit("Usage: python[3] " + sys.argv[0] + " <path to auto file>")
+
+in_path = pathlib.Path(sys.argv[1])
+if not in_path.exists():
+	raise SystemExit("Specified auto file does not exist!")
+
+with open(in_path, "r+") as file:
+	doc = json.load(file)
+	auto_cmds = doc["command"]["data"]["commands"]
+	for cmd in auto_cmds:
+		if cmd["type"] == "wait":
+			cmd["type"] = "named"
+			if cmd["data"]["waitTime"] == 1.5:
+				cmd["data"]["name"] = "l4"
+				del cmd["data"]["waitTime"]
+			elif cmd["data"]["waitTime"] == 2.5:
+				cmd["data"]["name"] = "intake"
+				del cmd["data"]["waitTime"]
+			
+	file.seek(0)
+	json.dump(doc, file, indent=2)	
+	
+print("Complete!")
---
+++ scripts/pathsubcorrectrot.py
@@ -0,0 +1,26 @@
+import json
+import sys
+import pathlib
+
+if len(sys.argv) < 2:
+	raise SystemExit("Usage: python[3] " + sys.argv[0] + " <path to auto file>")
+
+in_path = pathlib.Path(sys.argv[1])
+if not in_path.exists():
+	raise SystemExit("Specified path file does not exist!")
+
+with open(in_path, "r+") as file:
+	doc = json.load(file)
+	rot = doc["idealStartingState"]["rotation"]
+	rot -= 180
+	while rot < 0 or rot > 360:
+		if rot < 0:
+			rot += 360
+		else:
+			rot -= 360
+	
+	doc["idealStartingState"]["rotation"] = rot
+	file.seek(0)
+	json.dump(doc, file, indent=2)	
+	
+print("Complete!")
