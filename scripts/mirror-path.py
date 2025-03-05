a6276983edc9c7f40f076143c091b71e1e059542
bforcum <113265535+bforcum@users.noreply.github.com>
2025-03-05 15:38:43 -0800

    Consistent waypoint naming

--- scripts/mirror-path.py
+++ scripts/mirror-path.py
@@ -2,6 +2,23 @@ import sys
 import json
 import pathlib
 
+locationMirror = {
+  "S1": "S6",
+  "S2": "S5",
+  "S3": "S4",
+  "S4": "S3",
+  "S5": "S2",
+  "S6": "S1",
+  "D1": "D6",
+  "D2": "D5",
+  "D3": "D4",
+  "D4": "D3",
+  "D5": "D2",
+  "D6": "D1",
+  "P1": "P2",
+  "P2": "P1"
+}
+
 if len(sys.argv) < 3:
 	raise SystemExit("Usage: python[3] " + sys.argv[0] + " <path to auto file or directory> <path to write output>")
 
@@ -15,12 +32,23 @@ def mirror(inPath):
   file = open(inPath)
   
   doc = json.load(file)
+
+  for waypoint in doc["waypoints"]:
+    waypoint["anchor"]["y"] = 8 - waypoint["anchor"]["y"]
+    if not waypoint["prevControl"] is None:
+      waypoint["prevControl"]["y"] = 8 - waypoint["prevControl"]["y"]
+    if not waypoint["nextControl"] is None:
+      waypoint["nextControl"]["y"] = 8 - waypoint["nextControl"]["y"]
   
+  doc["goalEndState"]["rotation"] *= -1
+
+  doc["idealStartingState"]["rotation"] *= -1
   
+  return doc 
   
 try:
   outfile = open(out_path, "x")
 except:
-  raise SystemExit("Specified write path already exists")
+  raise SystemExit("Specified out path already exists")
 
-json.dump(mirror(in_path), outfile)
+json.dump(mirror(in_path), outfile, indent=2)
