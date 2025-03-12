e7728fe9f51a3ab272613bbc6fc15ca63fda2ea4
Aceius E. <aceiuselms@seattleacademy.org>
2025-03-12 16:25:11 -0700

    Implement lazer can sensor and subsystem

---
+++ src/main/java/frc/robot/subsystems/rangefinder/Rangefinder.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.rangefinder;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.Optional;
+
+import org.littletonrobotics.junction.Logger;
+
+public class Rangefinder extends SubsystemBase {
+  private final RangefinderIO io;
+  private final RangefinderIOInputsAutoLogged inputs = new RangefinderIOInputsAutoLogged();
+
+  public Rangefinder(RangefinderIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Rangefinder", inputs);
+  }
+
+  /** Get the measurement in millimetres */
+  public Optional<Integer> getMeasurement() {
+    return io.getMeasurementMillimetres();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/rangefinder/RangefinderIOHardware.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.rangefinder;
+
+import au.grapplerobotics.LaserCan;
+import java.util.Optional;
+
+public class RangefinderIOHardware implements RangefinderIO {
+  LaserCan sensor;
+
+  int measurementMillimetres;
+
+  public RangefinderIOHardware(int id) {
+    sensor = new LaserCan(id);
+  }
+
+  @Override
+  public void updateInputs(RangefinderIOInputs inputs) {
+    inputs.measurementMillimetres = getMeasurementMillimetres().orElse(-1);
+  }
+
+  @Override
+  public Optional<Integer> getMeasurementMillimetres() {
+    LaserCan.Measurement measurement = sensor.getMeasurement();
+
+    if (measurement != null && measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT) {
+      return Optional.of(measurement.distance_mm);
+    } else {
+      return Optional.empty();
+    }
+  }
+}
