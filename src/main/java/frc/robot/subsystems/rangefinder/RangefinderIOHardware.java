649974add6192bc24854374363df62e46849ec32
Aceius E. <aceiuselms@seattleacademy.org>
2025-03-12 16:32:05 -0700

    spotless

--- src/main/java/frc/robot/subsystems/rangefinder/RangefinderIOHardware.java
+++ src/main/java/frc/robot/subsystems/rangefinder/RangefinderIOHardware.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 public class RangefinderIOHardware implements RangefinderIO {
   LaserCan sensor;
 
-  int measurementMillimetres;
-
   public RangefinderIOHardware(int id) {
     sensor = new LaserCan(id);
   }
